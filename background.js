984dfbd28360b5978ee5d3faa9fcbe0d50ff4192
Tanmaya Kumar <tanmayakg@gmail.com>
2019-03-15 03:50:54 -0500

    Feat: removed popup files, replaced with alerting in babkground js. Currently able to query is a website is up based on a selection of any word on page

--- background.js
+++ background.js
@@ -2,7 +2,27 @@ console.log('chrome ext background script');
 
 chrome.runtime.onMessage.addListener(receiver);
 function receiver(request, sender, sendResponse) {
+  var alert = "";
+  var url = "";
+
   word = request.text;
-  console.log(word);
-  window.open("popup.html", "extension_popup", "width=300,height=400,status=no,scrollbars=yes,resizable=no");
+  chrome.tabs.query({
+    active: true,
+    currentWindow: true
+  }, function (tabs) {
+    url = new URL(tabs[0].url).hostname;
+    // alert("Inner Message: " + url);
+    fetch('https://downforeveryoneorjustme.com/' + url)
+      .then(function (response) {
+        var retValue = response.text();
+        return retValue;
+      })
+      .then(function (respBody) {
+        if (respBody.includes('It\'s just you.')) {
+          this.alert(url + " is Not Down for everyone, just you.");
+        } else if (respBody.includes('It\'s not just you!')) {
+          this.alert(url + " is Down for everyone.");
+        }
+      });
+  });
 }
--- popup.js
+++
@@ -1,3 +0,0 @@
-let bgpage = chrome.extension.getBackgroundPage();
-let word = bgpage.word.trim();
-document.getElementById("url").value = word;
